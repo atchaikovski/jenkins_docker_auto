db70a22df78a65bc516f18c99936267f47f103ac
Alex Tchaikovski <alex@tchaikovski.net>
2021-10-24 11:17:28 +0300

    fixed jdk.groovy

--- groovy/jdk.groovy
+++ groovy/jdk.groovy
@@ -5,8 +5,10 @@ import hudson.tools.ZipExtractionInstaller
 def descriptor = new JDK.DescriptorImpl();
 def List<JDK> installations = []
 
-javaTools=[['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u131-linux-x64.tar.gz', 'subdir':'jdk1.8.0_131'],
-           ['name':'jdk9', 'url':'file:/var/jenkins_home/downloads/openjdk-9.0.1_linux-x64_bin.tar.gz', 'subdir':'jdk1.8.0_131']
+javaTools=[
+           ['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u131-linux-x64.tar.gz', 'subdir':'jdk1.8.0_131'],
+           ['name':'jdk7', 'url':'file:/var/jenkins_home/downloads/openjdk-7u75-b13-linux-x64-18_dec_2014.tar.gz', 'subdir':'jdk1.7'],
+           ['name':'jdk9', 'url':'file:/var/jenkins_home/downloads/openjdk-9.0.1_-linux-x64_bin.tar.gz', 'subdir':'jdk1.9']
           ]
 
 javaTools.each { javaTool ->
